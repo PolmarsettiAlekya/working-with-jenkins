74a1653f4ea108a9a2a1385b48a5a732683ec085
PolmarsettiAlekya <palekya9933992732@gmail.com>
2024-02-02 10:32:28 +0530

    changes

---
+++ App5.java
@@ -0,0 +1,8 @@
+public class App5{
+public static void main(String[] args){
+System.out.println("Java App Pulled & Build by Jenkins");
+System.out.println("Hello Java and Jenkins");
+System.out.println("Hello World");
+
+}
+}
